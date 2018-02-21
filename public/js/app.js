ebb41a018b5061b3c91f36595bda517e72bc6d7a
MarinaRH <l_marina1717@hotmail.com>
2018-02-21 05:44:49 -0500

    agregando postImage

--- public/js/app.js
+++ public/js/app.js
@@ -26,3 +26,59 @@ $(document).ready(function() {
     close: 'Ok',
     closeOnSelect: false // Close upon selecting a date,
   });
+
+  var $chart = $('#btn-chart');
+  var $file = $('#btn-file');
+  var $video = $('#btn-video');
+  var $evento = $('#btn-evento');
+
+  $chart.click(charts);
+
+  function charts() {
+    $('#modal1').modal('close');
+    $inputVal = $('#modal1 input:text').val();
+    $textareaVal = $('#textarea1').val();
+
+    $('main').append(
+      `<div class="row">
+        <div class="col s12 m8 offset-m2">
+          <div class="card horizontal hoverable">
+            <div class="card-stacked">
+              <div class="card-content center-align">
+                <h4>${$inputVal}</h4>
+                <p>${$textareaVal}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>`);
+  }
+
+  $('#filesimg').change(function() {
+    var filest = (this.files[0].name).toString();
+    var fileReader = new FileReader();
+    fileReader.onload = function(e) {
+      $('#modal2 img').attr('src', e.target.result);
+    };
+    fileReader.readAsDataURL(this.files[0]);
+  });
+
+  function postImagen() {
+    $inputVal2 = $('#modal2 input:text').val();
+    let imgSrc = $('#thumb');
+    $('#modal2').modal('close');
+    $('main').append(`<div class="row">
+              <div class="col s12 m8 offset-m2">
+                <div class="card horizontal hoverable">
+                  <div class="card-stacked">
+                    <div class="card-content col m12">
+                      <h4>${inputVal2}</h4>
+                      <img src="${imgSrc[0].src}">
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>`);
+  }
+
+  $file.on('click', postImagen);
